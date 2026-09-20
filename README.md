050563b8f176df22917be706a9d89c3c35ae966d
Kyle Klus <kyle.klus.2@gmail.com>
2026-09-20 17:22:46 +0200

    INIT: Initialized dev workflow & file base

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # printable-calendar-generator
-This is a small web app which can generate a yearly calendar based on the users wishes
+
+This is a small web app which can generate a yearly/monthly calendar based on the users wishes.
+
+## Features
+
+- Customizable colors for each month
+- Customizable width for a month (number of cells for each day to enter events)
+- Adjustable layout
+  - Number of columns that fit on one A4 sheet
+  - Number of rows on one A4 sheet
+- CW next to start of week
+- Weekends are colored in
+
+## Feature Ideas for the next version
+
+- Customizable start of week
+- Setting a background
+- Custom fields
+- Adding german as a language option
+- Adding country specific holidays
+
+## Design choices
+
+- No / almost no dependencies
+- Written in typescript
+- Using luxon for date & time code
+- I am trying out a cartoony design
+  - Buttons pop out by having a 2x border at the bottom
+  - Hard shadows
+  - Handwritten but still legible font -> [Itim](https://fontsource.org/fonts/itim)
---
+++ webpack.config.ts
@@ -0,0 +1,63 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import CopyPlugin from 'copy-webpack-plugin';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: './src/main.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    devtool: 'source-map',
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 8080,
+        open: true,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: './src/public',
+                    to: '',
+                },
+                {
+                    from: './src/style.css',
+                    to: 'style.css',
+                },
+                {
+                    from: './src/index.html',
+                    to: 'index.html'
+                }
+            ],
+        }),
+    ],
+};
+
+export default config;
